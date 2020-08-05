5e468237cd4d4f379076a0d026d3627731f3fa3d
Kotovich I <zherebtsovairina@mail.ru>
2020-08-05 16:31:11 +0300

    убрала строчку с ошибкой

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Привет!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply(''));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
