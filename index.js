91ed19945c5897d57222a3ff7b05fef4ebcd1ce6
Kotovich I <zherebtsovairina@mail.ru>
2020-08-06 09:10:30 +0300

    Добавила port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const Telegraf = require('telegraf');
+const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Привет!'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+bot.start(ctx => ctx.reply("Привет!"));
+bot.help(ctx => ctx.reply("Send me a sticker"));
+bot.hears("hi", ctx => ctx.reply("Hey there"));
+bot.launch({
+  webhook: {
+    domain: "https://tfs-telegrem-bot.herokuapp.com/",
+    port: process.env.PORT
+  }
+});
