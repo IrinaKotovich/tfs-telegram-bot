0f7a0c67e23cbcda07aa0be2af01f51c3e426cf4
Kotovich I <zherebtsovairina@mail.ru>
2020-08-06 09:32:57 +0300

    правик

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ bot.launch({
     port: process.env.PORT
   }
 });
+bot.on('inline_query', (ctx) => {
+  const result = []
+  // Explicit usage
+  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+
+  // Using context shortcut
+  ctx.answerInlineQuery(result)
+})
