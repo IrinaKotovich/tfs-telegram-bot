c7741f91d22f3cdccb32f6fda4d59f6fce6354d6
Kotovich I <zherebtsovairina@mail.ru>
2020-08-07 14:41:22 +0300

    правик

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start(ctx => ctx.reply("Привет!"));
+/*bot.start(ctx => ctx.reply("Привет!"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
-bot.hears("hi", ctx => ctx.reply("Hey there"));
+bot.hears("hi", ctx => ctx.reply("Hey there"));*/
 bot.launch({
   webhook: {
     domain: "https://tfs-telegrem-bot.herokuapp.com/",
     port: process.env.PORT
   }
 });
-bot.on('inline_query', (ctx) => {
-  const result = []
-  // Explicit usage
-  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+bot.start(ctx => {
+  console.log("default chats to notify:", chatsToNotify);
 
-  // Using context shortcut
-  ctx.answerInlineQuery(result)
-})
+  ctx.reply(
+    "Используйте команду /subscribe, чтобы подписаться на уведомление об изменениях"
+  );
+});
+
+bot.command("/subscribe", ctx => {
+  console.log("ctx", ctx);
+  const { id: chatId } = ctx.chat || {};
+
+  console.log("new chatId:", chatId);
+
+  if (!chatsToNotify.includes(chatId)) {
+    chatsToNotify.push(chatId);
+  }
+  ctx.reply("Вы успешно подписались");
+});
+
+bot.on("text", async function(ctx) {
+  const { publisherId, message: { text, html } = {}, resource: { url } = {} } =
+  ctx.update || {};
+  if (publisherId !== "tfs") {
+    return;
+  }
+
+  console.log("ctx", ctx);
+  console.log("ctx.update", ctx.update);
+
+  try {
+    chatsToNotify.forEach(async chatId => {
+      await telegram.sendMessage(chatId, `HTML:${html}`, {
+        parse_mode: "HTML"
+      });
+    });
+  } catch (e) {
+    console.error("error", e);
+  }
+});
+
+bot.telegram.setWebhook(
+  "https://tfs-observer-telegram-bot.herokuapp.com/telegraf/07e4f521f4a38e9e50e08b3f8525efe23fc556fa9b6cb75ad2b987a612fce3e9"
+);
+
+expressApp.get("/", (req, res) => res.send("Hello World!"));
+expressApp.use(
+  bot.webhookCallback(
+    "/telegraf/07e4f521f4a38e9e50e08b3f8525efe23fc556fa9b6cb75ad2b987a612fce3e9"
+  )
+);
+
+expressApp.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}!`);
+});
