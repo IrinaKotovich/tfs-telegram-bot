5b08ade1ab349c01b985fee5c17cb50e63d40392
Kotovich I <zherebtsovairina@mail.ru>
2020-08-07 14:48:06 +0300

    правик

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-/*bot.start(ctx => ctx.reply("Привет!"));
-bot.help(ctx => ctx.reply("Send me a sticker"));
-bot.hears("hi", ctx => ctx.reply("Hey there"));*/
+bot.start(ctx => ctx.reply("Привет!"));
+
 bot.launch({
   webhook: {
     domain: "https://tfs-telegrem-bot.herokuapp.com/",
