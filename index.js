ac21c4042b896a08505c887996fcb5b3d719ee92
Kotovich I <zherebtsovairina@mail.ru>
2020-08-09 10:37:27 +0300

    правик

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ bot.on("text", async function(ctx) {
   }
 });
 
-bot.telegram.setWebhook(
+/*bot.telegram.setWebhook(
   "https://tfs-telegram-bot.herokuapp.com/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322"
-);
+);*/
 
 /*expressApp.get("/", (req, res) => res.send("Hello World!"));
 expressApp.use(
