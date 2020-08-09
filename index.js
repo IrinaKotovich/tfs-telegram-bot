180be94a570b073428f1b0fe0d19066c31dbdd3d
Kotovich I <zherebtsovairina@mail.ru>
2020-08-09 10:42:32 +0300

    правик

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ bot.on("text", async function(ctx) {
   }
 });
 
-/*bot.telegram.setWebhook(
+bot.telegram.setWebhook(
   "https://tfs-telegram-bot.herokuapp.com/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322"
-);*/
+);
 
-/*expressApp.get("/", (req, res) => res.send("Hello World!"));
+expressApp.get("/", (req, res) => res.send("Hello World!"));
 expressApp.use(
   bot.webhookCallback(
     "/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322"
@@ -62,11 +62,11 @@ expressApp.use(
 
 expressApp.listen(PORT, () => {
   console.log(`app listening on port ${PORT}!`);
-});*/
+});
 
-bot.launch({
+/*bot.launch({
   webhook: {
     domain: 'https://tfs-telegram-bot.herokuapp.com/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322',
     port: PORT
   }
-});
+});*/
