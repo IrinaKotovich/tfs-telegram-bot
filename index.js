d41947b9d943dd550cb4958ea1d4f4d8c52c453f
Kotovich I <zherebtsovairina@mail.ru>
2020-08-09 10:49:06 +0300

    правик

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ bot.telegram.setWebhook(
   "https://tfs-telegram-bot.herokuapp.com/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322"
 );
 
-expressApp.get("/", (req, res) => res.send("Hello World!"));
+/*expressApp.get("/", (req, res) => res.send("Hello World!"));
 expressApp.use(
   bot.webhookCallback(
     "/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322"
@@ -62,6 +62,11 @@ expressApp.use(
 
 expressApp.listen(PORT, () => {
   console.log(`app listening on port ${PORT}!`);
-});
+});*/
 
-bot.launch();
+bot.launch({
+  webhook: {
+    domain: 'https://tfs-telegram-bot.herokuapp.com',
+    port: PORT
+  }
+});
