1d4d9faa574570aea55735ebb17e7d68717d7b03
Kotovich I <zherebtsovairina@mail.ru>
2020-08-09 10:37:02 +0300

    правик

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ expressApp.listen(PORT, () => {
 
 bot.launch({
   webhook: {
-    domain: "https://tfs-telegram-bot.herokuapp.com/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322",
+    domain: 'https://tfs-telegram-bot.herokuapp.com/telegraf/7a2548463a4cafdb30128c2d72de871026a25c2ed0b491496a1366f071e96322',
     port: PORT
   }
 });
